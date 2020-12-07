a1e1c8ce9b82b13f22a32e781f883aec215bf039
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-12-07 12:34:32 +0530

    Update arr_chunk.js

--- arr_chunk.js
+++ arr_chunk.js
@@ -33,21 +33,35 @@ chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 // Solution - 2
 
-function arrChunk(arr, length) {
-    let subrr = [];
-    let output = [];
-    for (let num in arr) {
-        subrr.push(arr[num]);
-        if (subrr.length == length) {
-            output.push(subrr);
-            subrr = [];
-        } else {
-            if (num == (arr.length - 1)) {
-                output.push(subrr)
-            }
-        }
+// function arrChunk(arr, size) {
+//     const chunked = [];
+
+//     for (let element of arr) {
+//         const last = chunked[chunked.length - 1];
+//         console.log(last);
+//         if (!last || last.length === size) {
+//             chunked.push([element]);
+//         }else{
+//             last.push(element)
+//         }
+//     }
+//     return chunked;
+// }
+
+// console.log(arrChunk([1, 2, 3, 4, 5, 7, 8, 9], 3));
+
+// Solution - 3
+
+function arrChunk(arr, size) {
+    const chunked = [];
+    let index = 0;
+
+    while (index < arr.length) {
+        chunked.push(arr.slice(index, index + size));
+        index += size
     }
-    return output;
+
+    return chunked;
 }
 
 console.log(arrChunk([1, 2, 3, 4, 5, 7, 8, 9], 3));
