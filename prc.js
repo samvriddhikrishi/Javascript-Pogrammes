f6092ac57e10de0d68f3d7772b3c473398d18603
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-12-08 11:39:36 +0530

    Update prc.js

--- prc.js
+++ prc.js
@@ -1,7 +1,14 @@
 function moves(arr) {
-    const len = arr.sort(function (a, b) {return a > b});
-    return len;
-
+    const len = arr.sort(function (a, b) { return a > b });
+    let moves = 0;
+    for (let i = len.length; i >= 0; i--) {
+        if (i > len[i - 1]) {
+            moves += (i - len[i - 1])
+        } else if (i < len[i - 1]) {
+            moves += (len[i - 1] - i)
+        }
+    }
+    return moves;
 }
 
-console.log(moves([-1,2,10]));
+console.log(moves([1, 2, 1, 4]));
