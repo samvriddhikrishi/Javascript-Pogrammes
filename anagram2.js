1f0fb49a27f35facc4c8dd3cb4583576f9e55a82
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2021-02-17 20:08:57 +0530

    Create anagram2.js

---
+++ anagram2.js
@@ -0,0 +1,22 @@
+// find all the anagrams is number of each chracter in both strings should be same
+
+let arr = ['abc', 'cba', 'dell', 'leld', 'boo'];
+
+let obj = {};
+
+for (let i = 0; i < arr.length; i++) {
+    let word = arr[i].split("").sort().join("")
+    if (obj[word]) {
+        obj[word].push(arr[i])
+    } else {
+        obj[word] = [];
+        obj[word].push(arr[i])
+    }
+}
+
+const keys = Object.keys(obj);
+for (let j = 0; j < keys.length; j++) {
+    if (obj[keys[j]].length > 1){
+        console.log(obj[keys[j]])
+    }
+}
