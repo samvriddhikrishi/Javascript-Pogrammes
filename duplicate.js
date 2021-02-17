9046b19999f941ffd6d95a3389c2aa291d9c7068
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2021-02-17 19:14:08 +0530

    Update duplicate.js

--- duplicate.js
+++ duplicate.js
@@ -1,29 +1,48 @@
-/*arr = [1,3,5,6,3,4,5];
+////// Solution - 1
 
-console.log([...new Set(arr)]); */
+arr = [1, 3, 5, 6, 3, 4, 5];
 
-////////////////////////             OR               ///////////////////
+console.log([...new Set(arr)]);
 
-a = [1,3,2,3,4,1,4,5,3,2,5];
+
+
+///// Solution - 2
+
+a = [1, 3, 2, 3, 4, 1, 4, 5, 3, 2, 5];
 
 b = [];
 
-b[0]=a[0]; 
+b[0] = a[0];
 
-for(var i=1;i<a.length;i++){
-    for(var j=0;j<b.length;j++){
+for (var i = 1; i < a.length; i++) {
+    for (var j = 0; j < b.length; j++) {
 
-        if(a[i]==b[j]){
+        if (a[i] == b[j]) {
             break;
         }
-        else if(j != b.length-1){
+        else if (j != b.length - 1) {
             continue;
         }
-        else{
-            b[j+1]=a[i];
-           
+        else {
+            b[j + 1] = a[i];
+
         }
     }
 }
 
 console.log(b);
+
+
+// Solution - 3
+
+let arr = [1, 2, 3, 4, 2, 3, 4, 5, 6, 5, 7, 7];
+
+let output = [];
+
+for (let i = 0; i < arr.length; i++) {
+    if (output.includes(arr[i]) === false) {
+        output.push(arr[i])
+    }
+}
+
+console.log(output);
