0e4469b3aadd954bd85f37a43e6bba555e35a52e
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2021-02-25 11:34:10 +0530

    update

---
+++ app.js
@@ -0,0 +1,12 @@
+let arr = [1, 2, 3, 4, 5];
+let n = 4;
+let output;
+
+for (let i = 0; i < n; i++) {
+    let element= arr.shift();
+    output = arr;
+    output.push(element);
+    arr = output;
+}
+
+console.log(arr)
