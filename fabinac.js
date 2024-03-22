9a078526c031fda92fcc65c250ee74140a36a967
jpedakal <40006634+jpedakal@users.noreply.github.com>
2024-03-22 11:44:04 +0530

    Update fabinac.js

--- fabinac.js
+++ fabinac.js
@@ -1,3 +1,8 @@
+The Fibonacci series is a sequence of numbers where each number is the sum of the two preceding ones, usually starting with 0 and 1.
+
+example: 0,1,1,2,3,5,8,13,21,34,…
+
+
 let n1 = 0;
 let n2 = 1;
 let nextNum;
