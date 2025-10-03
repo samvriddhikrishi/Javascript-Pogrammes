36f1a284336b513faf5dde97b4dadc2836a4020a
jpedakal <jayakrishna490@gmail.com>
2025-10-03 12:31:26 +0530

    updated code

--- duplicate.js
+++ duplicate.js
@@ -1,12 +1,14 @@
-////// Solution - 1
-arr = [1, 3, 5, 6, 3, 4, 5];
+// Solution - 1
+let arr = [1, 3, 5, 6, 3, 4, 5];
 
 console.log([...new Set(arr)]);
 
-///// Solution - 2
-a = [1, 3, 2, 3, 4, 1, 4, 5, 3, 2, 5];
 
-b = [];
+
+// Solution - 2
+let a = [1, 3, 2, 3, 4, 1, 4, 5, 3, 2, 5];
+
+let b = [];
 
 b[0] = a[0];
 
@@ -29,13 +31,13 @@ for (var i = 1; i < a.length; i++) {
 console.log(b);
 
 // Solution - 3
-let arr = [1, 2, 3, 4, 2, 3, 4, 5, 6, 5, 7, 7];
+let array = [1, 2, 3, 4, 2, 3, 4, 5, 6, 5, 7, 7];
 
 let output = [];
 
-for (let i = 0; i < arr.length; i++) {
-    if (output.includes(arr[i]) === false) {
-        output.push(arr[i])
+for (let i = 0; i < array.length; i++) {
+    if (!output.includes(array[i])) {
+        output.push(array[i])
     }
 }
 
